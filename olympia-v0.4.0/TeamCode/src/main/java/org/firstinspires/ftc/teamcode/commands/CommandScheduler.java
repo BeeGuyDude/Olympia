b05add138b6f018c289f981f2930e7b88495a6df
Cian Capacci <46614295+BeeGuyDude@users.noreply.github.com>
2021-02-18 08:35:02 -0700

    LoopedCommand functionalist added, working on Sequential/Parallel rework

--- olympia-v0.4.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/CommandScheduler.java
+++ olympia-v0.4.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/CommandScheduler.java
@@ -1,5 +1,44 @@
 package org.firstinspires.ftc.teamcode.commands;
 
+import java.util.ArrayList;
+
 public class CommandScheduler {
-    
+    private ArrayList<Command> uninitialized;
+    private ArrayList<Command> loopedList;
+    private ArrayList<Command> parallelList;
+    private ArrayList<Command> sequentialList;
+
+    public void addLooped(Command command) {
+        uninitialized.add(command);
+        loopedList.add(command);
+    }
+
+    public void addParallel(Command command) {
+        uninitialized.add(command);
+        parallelList.add(command);
+    }
+
+    public void addSequential(Command command) {
+        sequentialList.add(command);
+    }
+
+    public void run() {
+        for (Command command : uninitialized) {
+            command.initialize();
+            uninitialized.remove(command);
+        }
+
+        for (Command command : loopedList) {
+            command.execute();
+        }
+
+    }
+
+    public void end() {
+        for (Command command : loopedList) {
+            command.end();
+            parallelList.remove(command);
+        }
+
+    }
 }

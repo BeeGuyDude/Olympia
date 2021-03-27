01993e1424975c8510b43381b11b2071b0d6252d
Cian Capacci <46614295+BeeGuyDude@users.noreply.github.com>
2021-03-27 16:31:13 -0600

    Partial implementation of command interrupt logic

--- olympia-v0.4.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/CommandScheduler.java
+++ olympia-v0.4.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/CommandScheduler.java
@@ -4,11 +4,9 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.commands.basecommands.Command;
 import org.firstinspires.ftc.teamcode.mechanisms.Mechanism;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 
@@ -18,17 +16,19 @@ public class CommandScheduler {
         LOW,
         HIGH
     }
-    private Map<Mechanism, ArrayList<Command>> mechanismBindingMap = new HashMap<Mechanism, ArrayList<Command>>();
-    private Map<Command, Boolean> boundCommandRunning = new HashMap<Command, Boolean>();
+
+    private ArrayList<Command> rawCommandList = new ArrayList<Command>();
     private Map<Command, commandPriority> boundCommandPriority = new HashMap<Command, commandPriority>();
 
-    private ArrayList<Command> commandList = new ArrayList<Command>();
-    private ArrayList<Command> executionList = new ArrayList<Command>();
+    private Map<Mechanism, ArrayList<Command>> mechanismBindingMap = new HashMap<Mechanism, ArrayList<Command>>();
+
+    private ArrayList<Command> commandExecutionList = new ArrayList<Command>();
     private ArrayList<Boolean> commandInitializedList = new ArrayList<Boolean>();
 
+    private Map<Command, Command> commandTriggeredEndingMap = new HashMap<Command, Command>();
+
     public void add(Command command) {
-        commandList.add(command);
-        commandInitializedList.add(false);
+        rawCommandList.add(command);
 
         if (!command.getBoundMechanisms().isEmpty()) {
             for (Mechanism mechanism : command.getBoundMechanisms()) {
@@ -38,14 +38,13 @@ public class CommandScheduler {
                     mechanismBindingMap.put(mechanism, new ArrayList(Arrays.asList(command)));
                 }
 
-                boundCommandRunning.put(command, false);
                 boundCommandPriority.put(command, commandPriority.LOW);
             }
         }
     }
 
     public void addHighPriority(Command command) {
-        commandList.add(command);
+        rawCommandList.add(command);
         commandInitializedList.add(false);
 
         if (!command.getBoundMechanisms().isEmpty()) {
@@ -56,7 +55,6 @@ public class CommandScheduler {
                     mechanismBindingMap.put(mechanism, new ArrayList(Arrays.asList(command)));
                 }
 
-                boundCommandRunning.put(command, false);
                 boundCommandPriority.put(command, commandPriority.HIGH);
             }
         }
@@ -64,12 +62,12 @@ public class CommandScheduler {
 
     public void run() {
 
-        if (!commandList.isEmpty()) {
-            for (Command command : commandList) {
+        if (!rawCommandList.isEmpty()) {
+            for (Command command : rawCommandList) {
 
-                if (commandInitializedList.get(commandList.indexOf(command)) == false) {
+                if (commandInitializedList.get(rawCommandList.indexOf(command)) == false) {
                     command.initialize();
-                    commandInitializedList.set(commandList.indexOf(command), true);
+                    commandInitializedList.set(rawCommandList.indexOf(command), true);
                 }
 
                 command.execute();
@@ -77,25 +75,82 @@ public class CommandScheduler {
                 if (command.isFinished()) {
                     command.end();
 
-                    commandInitializedList.remove(commandList.indexOf(command));
-                    commandList.remove(command);
+                    commandInitializedList.remove(rawCommandList.indexOf(command));
+                    rawCommandList.remove(command);
+                }
+            }
+        }
+    }
+
+    public void updatedRun() {
+        if (!rawCommandList.isEmpty()) {
+
+            for (Command command : rawCommandList) {
+                boolean passedCheck = true;
+
+                if (command.getBoundMechanisms().isEmpty()) {
+                    if (commandExecutionList.contains(command)) {
+                        passedCheck = false;
+                    }
+                } else {
+                    for (Command cachedCommand : commandExecutionList) {
+                        for (Mechanism mechanism : cachedCommand.getBoundMechanisms()) {
+                            if (command.getBoundMechanisms().contains(mechanism)) {
+                                if (boundCommandPriority.get(command) == boundCommandPriority.get(cachedCommand) || boundCommandPriority.get(command) == commandPriority.HIGH) { //Successful Interrupt
+                                    commandTriggeredEndingMap.put(command, cachedCommand);
+                                } else { //Failed Interrupt
+                                    for (Map.Entry<Command, Command> endRequestEntry : commandTriggeredEndingMap.entrySet()) {
+                                        if (endRequestEntry.getKey() == command) {
+                                            commandTriggeredEndingMap.remove(endRequestEntry);
+                                        }
+                                    }
+
+                                    passedCheck = false;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (passedCheck) {
+                    commandExecutionList.add(command);
+                    commandInitializedList.add(false);
+                }
+            }
+        }
+
+        if (!commandExecutionList.isEmpty()) {
+            for (Command command : commandExecutionList) {
+
+                if (commandInitializedList.get(rawCommandList.indexOf(command)) == false) {
+                    command.initialize();
+                    commandInitializedList.set(rawCommandList.indexOf(command), true);
+                }
+
+                command.execute();
+
+                if (command.isFinished()) {
+                    command.end();
+
+                    commandInitializedList.remove(rawCommandList.indexOf(command));
+                    commandExecutionList.remove(command);
                 }
             }
         }
     }
 
     public void end() {
-        for (Command command : commandList) {
+        for (Command command : rawCommandList) {
             command.end();
         }
     }
 
     public boolean isEmpty() {
-        return commandList.isEmpty();
+        return rawCommandList.isEmpty();
     }
 
     public void postCommands(Telemetry telemetry) {
-        for (Command command : commandList) {
+        for (Command command : rawCommandList) {
             telemetry.addData(command.toString(), "Initialized");
         }
     }

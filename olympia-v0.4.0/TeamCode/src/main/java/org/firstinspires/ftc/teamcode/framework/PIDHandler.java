821926aa749850808d978573495edb2aa034ec85
Cian Capacci <46614295+BeeGuyDude@users.noreply.github.com>
2021-03-31 22:05:23 -0600

    Added Scope Modifiers to PIDHandler Class

--- olympia-v0.4.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/framework/PIDHandler.java
+++ olympia-v0.4.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/framework/PIDHandler.java
@@ -1,21 +1,21 @@
 package org.firstinspires.ftc.teamcode.framework;
 
-public class PIDHandler { //Only actually includes pid
-    double kP = 0;
-    double kI = 0;
-    double kD = 0;
-    double P, I = 0, D;
-    double error, previous_error;
+public class PIDHandler { //Only actually includes PID, no F
+    private double kP = 0;
+    private double kI = 0;
+    private double kD = 0;
+    private double P, I = 0, D;
+    private double error, previous_error;
 
-    boolean D_ran = false;
+    private boolean D_ran = false;
 
-    void SetConstants(double kP, double kI, double kD) {
+    public void setConstants(double kP, double kI, double kD) {
         this.kP = kP;
         this.kI = kI;
         this.kD = kD;
     }
 
-    double GetPID(double current, double target, double dt) {
+    public double getPID(double current, double target, double dt) {
         error = target - current;
         P = error * kP; //Compute P
         I += error * kI * dt; //Compute I
@@ -30,11 +30,11 @@ public class PIDHandler { //Only actually includes pid
         return P + I + D; //Add it all together
     }
 
-    void ResetI() {
+    public void resetI() {
         I = 0;
     }
 
-    void ResetD() {
+    public void resetD() {
         D_ran = false;
     }
 }
